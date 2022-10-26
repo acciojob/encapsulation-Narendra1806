cdc30fb5963cc5347e29529c38d8a0e5e234c0bc
Narendra More <narendramore71@gmail.com>
2022-10-26 14:09:11 +0530

    This is encapsulation example

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args){
+        RWOnly obj = new RWOnly();
+//        obj.name = "Bangalore";
+//        name has private access in com.driver.RWOnly
+        obj.setName("Pune");
+        System.out.println(obj.getName());
+    }
 }
